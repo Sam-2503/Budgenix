cfcff3cb89afcd2c67262f700eff982d23d838f4
kaihere14 <yashpalsharma9950@gmail.com>
2025-11-15 05:46:11 +0530

    fix: add missing file extensions for imports in gemini.routes.ts

--- server/src/routes/gemini.routes.ts
+++ server/src/routes/gemini.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import googleAi from "../controllers/gemini.controller";
-import { verifyJwt } from "../middleware/verifyJwt";
+import googleAi from "../controllers/gemini.controller.js";
+import { verifyJwt } from "../middleware/verifyJwt.js";
 const route = Router();
 
 route.post("/generateai",verifyJwt, googleAi);
